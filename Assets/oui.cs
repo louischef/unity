bf666e9654044347846a86de5e8f36b28581f944
Boulette3000 <63206718+Boulette3000@users.noreply.github.com>
2022-06-27 09:36:28 +0200

    True commit
    True commit

---
+++ Assets/oui.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oui : MonoBehaviour
+{
+    Animation baleine;
+
+    IEnumerator Anim()
+    {
+        baleine.Play("dive");
+
+        yield return new WaitForSeconds(baleine["Action"].length);
+
+ 
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        baleine = GetComponent<Animation>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (baleine.isPlaying)
+        {
+
+        }
+            
+        else
+        {
+            baleine.transform.Rotate(new Vector3(0, 180, 0), Space.Self);
+            baleine.Play("dive2");
+        }
+
+
+    }
+}
