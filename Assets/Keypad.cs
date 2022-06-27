33a5ada5a2bf4ebb9ea2e71a89295a3319760f96
Cessouille <celian.chausson@etu.univ-smb.fr>
2022-06-27 11:14:34 +0200

    oui
    oui

---
+++ Assets/Keypad.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keypad : MonoBehaviour
+{
+    [SerializeField] private Text Ans;
+    [SerializeField] private Animator Door;
+    [SerializeField] private Text Infos;
+
+    private string Answer = "2003";
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void Number(int number)
+    {
+        if (Ans.text == "Invalid")
+            Ans.text = "";
+        Ans.text += number.ToString();
+    }
+
+    public void Execute()
+    {
+        if(Ans.text == Answer)
+        {
+            Ans.text = "Correct";
+            Door.Play("door_3_open");
+            StartCoroutine("StopDoor");
+        }
+        else
+        {
+            Ans.text = "Invalid";
+        }
+    }
+
+    IEnumerator StopDoor()
+    {
+        yield return new WaitForSeconds(0.5f);
+        Door.enabled = false;
+        Infos.gameObject.SetActive(false);
+    }
+}
