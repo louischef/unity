d8ac87c2c7274b5c45c46495c36b99e3f4959cdd
louischef <107099943+louischef@users.noreply.github.com>
2022-06-28 17:55:39 +0200

    ^^
    ^^

---
+++ Assets/Bouton1.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bouton1 : MonoBehaviour
+{
+    public static bool activer = false;
+
+    [SerializeField] Text Infos;
+
+    public static GameObject bouton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bouton = GetComponent<GameObject>();
+
+        Infos.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!activer)
+            Infos.gameObject.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Infos.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E) && Infos.gameObject.active)
+        {
+            activer = true;
+            Infos.gameObject.SetActive(false);
+        }
+    }
+
+    public static bool Ratio()
+    {
+        return activer = false;
+    }
+}
