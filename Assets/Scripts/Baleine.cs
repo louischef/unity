f176bb8d922a8288d034ebe161538c9b67002d46
Cessouille <celian.chausson@etu.univ-smb.fr>
2022-06-28 14:02:03 +0200

    pp
    pp

---
+++ Assets/Scripts/Baleine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Baleine : MonoBehaviour
+{
+    [SerializeField] ThirdPersonCharacter baleine;
+    [SerializeField] GameObject waypoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        baleine.Move(waypoint.transform.position, false, false);
+    }
+}
