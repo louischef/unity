488c209973a87e53e585b093bbe51103601a0adf
louischef <107099943+louischef@users.noreply.github.com>
2022-06-28 10:18:01 +0200

    correction transition

--- Assets/porteTransition.cs
+++ Assets/porteTransition.cs
@@ -9,39 +9,30 @@ public class porteTransition : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField] private Animator Porte;
     [SerializeField] private Text Infos;
-    private Scene scene;
-    private AssetBundle myLoadedAssetBundle;
-    private string[] scenePaths;
     void Start()
     {
         Infos.gameObject.SetActive(false);
-        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/scenes");
-        scenePaths = myLoadedAssetBundle.GetAllScenePaths();
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
     private void OnTriggerEnter(Collider other)
     {
+        var parameters = new LoadSceneParameters(LoadSceneMode.Additive);
         Porte.Play("door_2_open");
         StartCoroutine("StopDoor");
         Infos.gameObject.SetActive(true);
+        
     }
-
 
     private void OnTriggerStay(Collider other)
     {
-        var parameters = new LoadSceneParameters(LoadSceneMode.Additive);
         if (Input.GetKeyDown(KeyCode.E) && Infos.gameObject.active)
-        {
             SceneManager.LoadScene("Scene2", LoadSceneMode.Single);
-            Infos.gameObject.SetActive(false);
-        }
-
     }
+
     private void OnTriggerExit(Collider other)
     {
         Porte.enabled = true;
